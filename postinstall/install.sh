a493996ef49d681b0409f98b2510d74368b6db00
FranGonza120 <franco20047@gmail.com>
2025-11-20 16:15:00 -0300

    Cambio el script install.sh para que cumpla con su prosito

--- postinstall/install.sh
+++ postinstall/install.sh
@@ -19,11 +19,84 @@ CONFIG_DIR=$HOME/.config
 sudo dnf -y update
 sudo dnf -y makecache
 
-./before/setup-particion.sh
-./before/setup-git.sh
+# Verificación de partición
+
+echo "Verificando que la partición Personal se encuentra montada correctamente"
+if [[ -d "$PARTICION_DIR" ]] && mountpoint -q "$HOME/Escritorio"; then
+	echo "✔ La partición Personal está montada correctamente"
+else
+	echo "La partición no se encuentra montada"
+	echo "Limpiando carpeta Escritorio"
+	mkdir -p "$HOME/Escritorio"
+	rm -rf $HOME/Escritorio/*
+
+	UUID=$(sudo blkid -s UUID -o value  "/dev/disk/by-label/Personal" || true)
+
+	# Verificación de que la partición se encuentra en el sistema
+	if [[ -z "$UUID" ]]; then
+		echo "No se encuentra la partición Personal en el sistema"
+		exit 1
+	fi
+	if ! grep -q "$UUID" /etc/fstab; then
+		MOUNTPOINT="$HOME/Escritorio"
+		echo "UUID=$UUID $MOUNTPOINT ext4 defaults 0 2" | sudo tee -a /etc/fstab > /dev/null
+	fi
+	sudo mount -U "$UUID" "$HOME/Escritorio"
+	echo "Se ha montado la partición Personal a la carpeta ~/Escritorio exitosamente"
+fi
+
+# Verificación y setup de git
+
+if command -v git >/dev/null 2>&1; then
+	echo "git se encuentra instalado"
+else
+	sudo dnf install -y git
+fi
+
+if [[ -f "$HOME/.ssh/id_ed25519.pub" ]]; then 
+	echo "La clave SSH está instalada"
+else 
+	echo "A presione enter en las opciones provistas por el generador de la clave ssh"
+	ssh-keygen -t ed25519 -C "franco20047@gmail.com"
+	eval "$(ssh-agent -s)"
+	ssh-add "$HOME/.ssh/id_ed25519"
+	echo "Agregue una nueva clave SSH en github y copie la siguiente clave:"
+	cat "$HOME/.ssh/id_ed25519.pub"
+	read -rp "Cuando agregue la clave y copie el contendido presione Enter." _
+	#Confirmación de conexióna github
+	echo "Confirmando la ssh-key con github.com"
+	echo "si pregunta sobre establecer conexion pone yes"
+	if ssh -T git@github.com 2>&1 | grep -q "successfully authenticated"; then
+	    echo "✔ SSH con GitHub funcionando (key OK)"
+	else
+	    echo "❌ No se pudo autenticar con GitHub vía SSH"
+	fi
+fi
+
+# Verificar dotfiles disponibles
+echo "Verificando que los dotfiles están disponibles"
+if [[ -d "$DOTFILES_DIR" ]]; then
+	echo "El repositorio se encuentra en la posición adecuada"
+	echo "Actualizando contenido del repositorio"
+	cd "$DOTFILES_DIR"
+	git fetch
+	if ! git diff --quiet; then
+	    echo "⚠️ Cambios locales detectados en dotfiles. No se puede hacer pull."
+	else
+	    git pull --ff-only
+	fi
+else
+	echo "El repositorio no se encuentra en la posición adecuada"
+	echo "Clonando repositorio dotfiles"
+	cd "$PARTICION_DIR"
+	git clone https://github.com/frangonza120/dotfiles
+	cd dotfiles
+fi
+# Activar scripts ejecutables
+echo "Activando scripts ejecutables de dotfiles"
+find "$DOTFILES_DIR" -name "*.sh" -exec chmod +x {} \;
 
 
-# verificar montado de partición Personal
 if [[ $# -ge 1 ]]; then
 	for arg in "$@"; do
 		case $arg in
