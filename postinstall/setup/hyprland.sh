782db8b1615919ca2833481286bc604c3cd5c21c
FranGonza120 <franco20047@gmail.com>
2026-03-17 10:25:14 -0300

    Cambios al install de hyprland para agregar filtro de luz azul

--- postinstall/setup/hyprland.sh
+++ postinstall/setup/hyprland.sh
@@ -4,7 +4,7 @@ if command -v hyprland >/dev/null 2>&1; then
 else
     echo "Instalando Hyprland y hyprpaper"
     sudo dnf copr enable solopasha/hyprland
-    sudo dnf install -y hyprland hyprpaper hyprpolkitagent hyprland-qtutils xdg-desktop-portal xdg-desktop-portal-hyprland
+    sudo dnf install -y hyprland hyprpaper hyprpolkitagent hyprland-qtutils xdg-desktop-portal xdg-desktop-portal-hyprland hyprsunset
     sudo dnf install -y blueman-applet mako waybar wofi fd brightnessctl grim wl-clipboard network-manager-applet unzip unrar
 fi
 echo "Creando links para carpetas de configuración de hyrpland, mako y waybar"
